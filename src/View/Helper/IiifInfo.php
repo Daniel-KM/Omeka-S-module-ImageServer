aab15db659c70cf0c2f9d5baa751c7557fdf103a
Daniel Berthereau <Daniel.git@Berthereau.net>
2020-10-26 00:00:00 +0000

    Fixed strict type issue.

--- src/View/Helper/IiifInfo.php
+++ src/View/Helper/IiifInfo.php
@@ -58,7 +58,7 @@ Object|null
         if (is_null($version)) {
             $version = $view->setting('imageserver_info_default_version', '2');
         } else {
-            $version = $version === '2' ? '2' : '3';
+            $version = (string) $version === '2' ? '2' : '3';
         }
 
         return $version === '2'
