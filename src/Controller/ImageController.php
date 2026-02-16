f7548f5423d7f67fd66bcfc3d9cefcca1ec017c6
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-02-16 00:00:00 +0000

    Checked access by range for audio/video.

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -321,8 +321,7 @@ class ImageController extends AbstractActionController
                     }
                 }
                 // Check valid range to avoid hack.
-                $hasRange = ($start < $filesize && $end < $filesize && $start < $end)
-                    && ($start > 0 || $end < ($filesize - 1));
+                $hasRange = $start >= 0 && $start <= $end && $end < $filesize;
             }
 
             if ($hasRange) {
